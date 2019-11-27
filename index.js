b7ad0b541bcb1fdfe9cd1540637c23a0124c3b74
Pharuxtan <alex360noscop@gmail.com>
2019-11-27 18:17:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,31 +8,6 @@ let frame;
 
 app.disableHardwareAcceleration();
 
-/* the binary Great Common Divisor calculator */
-function gcd (u, v) {
-    if (u === v) return u;
-    if (u === 0) return v;
-    if (v === 0) return u;
-
-    if (~u & 1)
-        if (v & 1)
-            return gcd(u >> 1, v);
-        else
-            return gcd(u >> 1, v >> 1) << 1;
-
-    if (~v & 1) return gcd(u, v >> 1);
-
-    if (u > v) return gcd((u - v) >> 1, v);
-
-    return gcd((v - u) >> 1, u);
-}
-
-/* returns an array with the ratio */
-function ratio (w, h) {
-	var d = gcd(w,h);
-	return [w/d, h/d];
-}
-
 app.on("ready", async () => {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
 
